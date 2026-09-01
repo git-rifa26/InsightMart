ae376c1d0d7960aacd4cae75b632cbc99347faab
rifakhan-hub <rifakhan3730@gmail.com>
2026-09-01 20:16:20 +0530

    login api's

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -3,6 +3,8 @@ from flask import Blueprint, request
 
 auth_bp = Blueprint("auth", __name__)
 
+
+
 @auth_bp.route("/auth/register/indivisual", methods=["POST"])
 def register():
 
@@ -39,9 +41,189 @@ def register():
         plan="free",
         organisation_id=None
     )
+    db.session.add(user)
+    db.session.commit()
+    
+    return {"message": "Validation Successful"}
 
+
+
+@auth_bp.route("/auth/register/enterprise", methods=["POST"])
+def register_enterprise():
+
+    from app.models.user import User
+    from app.models.organisation import Organisation
+    from app import db
+
+    data = request.get_json()
+
+    name = data.get("name")
+    email = data.get("email")
+    organisation_name = data.get("organisation_name")
+    password = data.get("password")
+    confirm_password = data.get("confirm_password")
+    terms = data.get("terms")
+
+    if not name or not email or not organisation_name:
+        return {"error": "Name, email and organisation name are required"}, 400
+
+    if not password or not confirm_password:
+        return {"error": "Password and confirm password are required"}, 400
+
+    if password != confirm_password:
+        return {"error": "Passwords do not match"}, 400
+
+    if not terms:
+        return {"error": "You must accept the terms"}, 400
+
+    existing_organisation = Organisation.query.filter_by(
+        name=organisation_name
+    ).first()
+
+    if existing_organisation:
+        return {"error": "Organisation already exists"}, 409
+
+    organisation = Organisation(
+        name=organisation_name
+    )
+    db.session.add(organisation)
+    db.session.flush()
+
+    user = User(
+            name=name,
+            email=email,
+            password_hash=password,
+            role="Organisation",
+            plan="free",
+            organisation_id=organisation.id
+        )
     db.session.add(user)
     db.session.commit()
 
-    
-    return {"message": "Validation Successful"}
+    return{
+        "message": "Enterprise account created successfully",
+    }, 201
+
+
+@auth_bp.route("/auth/login/indivisual", methods=["POST"])
+def login():
+
+    from app.models.user import User
+
+    data = request.get_json()
+
+    email = data.get("email")
+    password = data.get("password")
+
+    if not email or not password:
+        return {
+            "error": "Email and password are required"
+        }, 400
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user:
+        return {
+            "error": "Invalid email or password"
+        }, 401
+
+    if user.role != "individual":
+        return {
+            "error": "This account is not an Individual account"
+        }, 403
+
+    if user.password_hash != password:
+        return {
+            "error": "Invalid email or password"
+        }, 401
+
+    return {
+        "message": "Login successful",
+        "user_id": user.id,
+        "name": user.name,
+        "email": user.email,
+        "role": user.role,
+        "plan": user.plan
+    }, 200
+
+@auth_bp.route("/auth/login/organisation", methods=["POST"])
+def login_org():
+
+    from app.models.user import User
+
+    data = request.get_json()
+
+    email = data.get("email")
+    password = data.get("password")
+
+    if not email or not password:
+        return {
+            "error": "Email and password are required"
+        }, 400
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user:
+        return {
+            "error": "Invalid email or password"
+        }, 401
+
+    if user.role != "Organisation":
+        return {
+            "error": "This account is not an Organisation account"
+        }, 403
+
+    if user.password_hash != password:
+        return {
+            "error": "Invalid email or password"
+        }, 401
+
+    return {
+        "message": "Login successful",
+        "user_id": user.id,
+        "name": user.name,
+        "email": user.email,
+        "role": user.role,
+        "plan": user.plan
+    }, 200
+
+@auth_bp.route("/auth/login/admin", methods=["POST"])
+def login_admin():
+
+    from app.models.user import User
+
+    data = request.get_json()
+
+    email = data.get("email")
+    password = data.get("password")
+
+    if not email or not password:
+        return {
+            "error": "Email and password are required"
+        }, 400
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user:
+        return {
+            "error": "Invalid email or password"
+        }, 401
+
+    if user.role != "admin":
+        return {
+            "error": "This account is not an Organisation account"
+        }, 403
+
+    if user.password_hash != password:
+        return {
+            "error": "Invalid email or password"
+        }, 401
+
+    return {
+        "message": "Login successful",
+        "user_id": user.id,
+        "name": user.name,
+        "email": user.email,
+        "role": user.role,
+        "plan": user.plan
+    }, 200
